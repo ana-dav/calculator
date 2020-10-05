c81557da65a0cb78cf2603b98a8328160a1f664c
ana-dav <opa.super.op@gmail.com>
2020-10-05 23:35:06 +0300

    refactor: delete committed code

--- main.js
+++ main.js
@@ -87,39 +87,6 @@ class Calculator {
 
         this.upperScreenPart.innerText = this.currentOperand
     }
-
-    // getDisplayNumber(number) {
-    //     if(number === '-') return number.toString()
-    //     const stringNumber = number.toString()
-    //     const integerDigits = parseFloat(stringNumber.split('.')[0])
-    //     const decimalDigits = stringNumber.split('.')[1]
-    //     let integerDisplay
-    //     if (isNaN(integerDigits)) {
-    //         integerDisplay = ''
-    //       } else {
-    //         integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
-    //       }
-    //       if (decimalDigits != null) {
-    //         return `${integerDisplay}.${decimalDigits}`
-    //       } else {
-    //         return integerDisplay
-    //       }
-    // }
-
-    // updateDisplay() {
-    //     console.log(this.currentOperand)
-    //     this.upperScreenPart.innerText = 
-    //     this.getDisplayNumber(this.currentOperand)
-    //     if (this.operation === 'xn') {
-    //         this.lowerScreenPart.innerText = 
-    //         `${this.getDisplayNumber(this.previousOperand)} ^`
-    //     } else if (this.operation != null) {
-    //         this.lowerScreenPart.innerText = 
-    //         `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
-    //     } else {
-    //         this.lowerScreenPart.innerText = ''
-    //     }
-    // }
 }
 
 const numberButtons = document.querySelectorAll('[data-number]')
